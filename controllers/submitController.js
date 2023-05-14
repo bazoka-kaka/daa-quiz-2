bc0dfc22f88de2f8e96650e1b4f536df8f438e02
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-05-14 15:23:59 +0700

    add result page

--- controllers/submitController.js
+++ controllers/submitController.js
@@ -1,3 +1,5 @@
+const ejs = require("ejs");
+const path = require("path");
 const getShortestPath = require("./dijkstra");
 
 const handleSubmit = (req, res) => {
@@ -7,7 +9,17 @@ const handleSubmit = (req, res) => {
       .status(400)
       .json({ message: "Location source and destination must be specified!" });
   const routesResult = getShortestPath(location_from, location_destination);
-  res.json({ routesResult });
+  ejs.renderFile(
+    path.join(__dirname, "..", "views", "result.ejs"),
+    {
+      routesResult,
+      from: location_from,
+      destination: location_destination,
+    },
+    (err, str) => {
+      res.send(str);
+    }
+  );
 };
 
 module.exports = { handleSubmit };
